dff445e9fcf5c3eab9853a1a9e02658b6bf7467e
reynau <xavi.lopez.reynau@gmail.com>
2017-10-27 23:35:02 +0200

    New design

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("portfolio").style.height = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("portfolio").style.height = "0";
+}
