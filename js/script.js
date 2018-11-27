0b941b834db41c482887a3ea6def6fddb2814273
Xavier Lopez Reynau <xavi.lopez.reynau@gmail.com>
2018-11-27 02:43:21 +0100

    Start to use Sass and add carrousel for portfolio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+var html = document.getElementsByTagName("html")[0];
+var body = document.getElementsByTagName("body")[0];
+var h1 = document.getElementsByTagName("h1")[0];
+var menu = document.getElementById("main-menu");
+var navbutton = document.getElementById("nav-button");
+
+function navButtonAction (btn) {
+	btn.classList.toggle("close");
+
+	html.classList.toggle("white-background");
+	body.classList.toggle("dark-border");
+	menu.classList.toggle("menu-show");
+	navbutton.classList.toggle("dark-border");
+	navbutton.classList.toggle("dark-background")
+}
